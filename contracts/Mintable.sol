eb1737138a837fb16e0eb6d7b31ffc76ae2fadf9
Daniel Que <dmdque@gmail.com>
2020-11-23 10:36:26 -0800

    Add mirror invite token

---
+++ contracts/Mintable.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.6.8;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Mintable is Ownable, ERC20 {
+  event Mint(address indexed to, uint256 amount);
+  event MintFinished();
+
+  bool public mintingFinished = false;
+
+
+  modifier canMint() {
+    require(!mintingFinished);
+    _;
+  }
+
+  /**
+   * @dev Function to mint tokens
+   * @param to The address that will receive the minted tokens.
+   * @param amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(address to, uint256 amount) onlyOwner canMint public returns (bool) {
+    _mint(to, amount);
+    emit Mint (to, amount);
+    return true;
+  }
+
+  /**
+   * @dev Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+  function finishMinting() onlyOwner public returns (bool) {
+    mintingFinished = true;
+    MintFinished();
+    return true;
+  }
+
+  constructor(string memory name, string memory symbol) ERC20(name, symbol) public {
+  }
+}
---
+++ contracts/MirrorInviteToken.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.6.8;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Mintable.sol";
+import "./IENSManager.sol";
+
+contract MirrorInviteToken is Mintable {
+
+  address private _registrar;
+
+  constructor(string memory name, string memory symbol) Mintable(name, symbol) public {
+    _setupDecimals(0);
+  }
+
+  // ***** VIEW *****
+
+  function registrar() external view returns (address) {
+    return _registrar;
+  }
+
+  // ***** MUTATING FUNCTIONS *****
+
+  /**
+   * @dev Sets the registrar that the token interacts with for register
+   * @param registrar New registrar
+   */
+  function setRegistrar(address registrar) onlyOwner external {
+    _registrar = registrar;
+  }
+
+  /**
+   * @notice Helper to register subdomain without having to call approve. Calls register on the registrar contract.
+   * @param label The subdomain to register
+   * @param owner The owner to set
+   */
+  function register(string calldata label, address owner) external {
+    _approve(msg.sender, _registrar, 1);
+    IENSManager(_registrar).register(label, owner);
+  }
+}
