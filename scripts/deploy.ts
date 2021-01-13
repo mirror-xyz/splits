2bb1bb6530b4d5df67d73367500b3f474679dda8
Graeme Boy <graemelearn@protonmail.com>
2021-01-13 13:35:07 -0800

    update config

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,9 +4,16 @@ import fs from "fs";
 export const ZERO_BYTES32 = ethers.constants.HashZero;
 export const ROOT = "xyz";
 export const subnameWallet = "mirror";
+
+// For production
+// export const ENS_REGISTRY_ADDRESS = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e";
+// export const ROOT_NAME = 'mirror.xyz';
+// export const ROOT_NODE = ethers.utils.namehash(ROOT_NAME);
+
+// For test
 export const ENS_REGISTRY_ADDRESS = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e";
-export const ROOT_NAME = 'mirror.xyz';
-export const ROOT_NODE = ethers.utils.namehash(ROOT_NAME);
+export const ROOT_NAME = 'mirror.test'
+export const ROOT_NODE = ethers.utils.namehash(ROOT_NAME)
 
 const NETWORK_MAP = {
   '0': 'mainnet',
