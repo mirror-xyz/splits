e9a976f74ae075bb8257c20ffcee9a2965a3059c
nickadamson <naburner@protonmail.com>
2021-08-23 16:06:08 -0700

    change solc optimizer runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 2000
+            runs: 200
           }
         }
       },
@@ -24,7 +24,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 2000
+            runs: 200
           }
         }
       }
