217730e77b40254c669c0f5b64f6bb5ff65d768a
nickadamson <naburner@protonmail.com>
2021-08-28 21:36:21 -0700

    merge OurManagement contract

---
+++ contracts/OurManagement.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.4;
+
+contract OurManagement {
+  event AddedOwner(address owner);
+  event RemovedOwner(address owner);
+
+  address internal constant SENTINEL_OWNERS = address(0x1);
+
+  mapping(address => address) internal owners;
+  uint256 internal ownerCount;
+  uint256 internal threshold;
+
+  function requireSelfCall() private view {
+      require(msg.sender == address(this), "GS031");
+  }
+
+  modifier authorized() {
+      // This is a function call as it minimized the bytecode size
+      requireSelfCall();
+      _;
+  }
+
+  /// @dev Setup function sets initial owners of contract.
+  /// @param _owners List of Split owners (can mint/manage auctions)
+  function setupOwners(address[] memory _owners) internal {
+      // Threshold can only be 0 at initialization.
+      // Check ensures that setup function can only be called once.
+      require(threshold == 0, "Setup has already been completed once.");
+      // Initializing Safe owners.
+      address currentOwner = SENTINEL_OWNERS;
+      for (uint256 i = 0; i < _owners.length; i++) {
+          // Owner address cannot be null.
+          address owner = _owners[i];
+          require(owner != address(0) && owner != SENTINEL_OWNERS && owner != address(this) && currentOwner != owner, "GS203");
+          // No duplicate owners allowed.
+          require(owners[owner] == address(0), "GS204");
+          owners[currentOwner] = owner;
+          currentOwner = owner;
+      }
+      owners[currentOwner] = SENTINEL_OWNERS;
+      ownerCount = _owners.length;
+      threshold = 1;
+  }
+
+  /// @dev Allows to remove an owner 
+  /// @notice Removes the owner `owner` from the Split 
+  /// @param prevOwner Owner that pointed to the owner to be removed in the linked list
+  /// @param owner Owner address to be removed.
+  function removeOwner(
+      address prevOwner,
+      address owner,
+  ) public {
+      // Validate owner address and check that it corresponds to owner index.
+      require(owner != address(0) && owner != SENTINEL_OWNERS, "GS203");
+      require(owners[prevOwner] == owner, "GS205");
+      owners[prevOwner] = owners[owner];
+      owners[owner] = address(0);
+      ownerCount--;
+      emit RemovedOwner(owner);
+  }
+
+  /// @dev Allows to swap/replace an owner from the Split with another address.
+  /// @notice Replaces the owner `oldOwner` in the Split with `newOwner`.
+  /// @param prevOwner Owner that pointed to the owner to be replaced in the linked list
+  /// @param oldOwner Owner address to be replaced.
+  /// @param newOwner New owner address.
+  function swapOwner(
+      address prevOwner,
+      address oldOwner,
+      address newOwner
+  ) public {
+      // Owner address cannot be null, the sentinel or the Safe itself.
+      require(newOwner != address(0) && newOwner != SENTINEL_OWNERS && newOwner != address(this), "GS203");
+      // No duplicate owners allowed.
+      require(owners[newOwner] == address(0), "GS204");
+      // Validate oldOwner address and check that it corresponds to owner index.
+      require(oldOwner != address(0) && oldOwner != SENTINEL_OWNERS, "GS203");
+      require(owners[prevOwner] == oldOwner, "GS205");
+      owners[newOwner] = owners[oldOwner];
+      owners[prevOwner] = newOwner;
+      owners[oldOwner] = address(0);
+      emit RemovedOwner(oldOwner);
+      emit AddedOwner(newOwner);
+  }
+
+  function isOwner(address owner) public view returns (bool) {
+      return owner != SENTINEL_OWNERS && owners[owner] != address(0);
+  }
+
+  /// @dev Returns array of owners.
+  /// @return Array of Safe owners.
+  function getOwners() public view returns (address[] memory) {
+      address[] memory array = new address[](ownerCount);
+
+      // populate return array
+      uint256 index = 0;
+      address currentOwner = owners[SENTINEL_OWNERS];
+      while (currentOwner != SENTINEL_OWNERS) {
+          array[index] = currentOwner;
+          currentOwner = owners[currentOwner];
+          index++;
+      }
+      return array;
+  }
+}
--- contracts/OurProxy.sol
+++ contracts/OurProxy.sol
@@ -6,7 +6,6 @@ import { OurStorage } from "./OurStorage.sol";
 interface IOurFactory {
   function splitter() external returns (address);
   function minter() external returns (address);
-  function splitOwner() external returns (address);
   function merkleRoot() external returns (bytes32);
 }
 
@@ -17,15 +16,10 @@ interface IOurFactory {
  * @notice Modified: added OpenZeppelin's Ownable (modified)
  */
 contract OurProxy is OurStorage {
-  /// OZ Ownable.sol
-  address private _owner;
-
   constructor() {
+    _pylon = IOurFactory(msg.sender).pylon();
     _splitter = IOurFactory(msg.sender).splitter();
     _minter = IOurFactory(msg.sender).minter();
-
-    /// @notice tx.origin should not be used in the constructor as the deploy transaction can be front-run.
-    _owner = IOurFactory(msg.sender).splitOwner();
 
     merkleRoot = IOurFactory(msg.sender).merkleRoot();
 
@@ -33,42 +27,6 @@ contract OurProxy is OurStorage {
       abi.encodeWithSignature("setApprovalsForSplit(address)", owner())
     );
   }
-
-  //======== OpenZeppelin =========
-  event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
-
-  /// @notice Transfers ownership of the contract to a new account (`newOwner`).
-  function transferOwnership(address newOwner) public {
-    require(msg.sender == owner());
-    require(newOwner != address(0), "Ownable: new owner is the zero address");
-
-    _setOwner(newOwner);
-  }
-
-  /**
-   * @dev Leaves the contract without owner. It will not be possible to call
-   * `onlyOwner` functions anymore. Can only be called by the current owner.
-   *
-   * NOTE: Renouncing ownership will leave the contract without an owner,
-   * thereby removing any functionality that is only available to the owner.
-   */
-  function renounceOwnership() public {
-    require(msg.sender == owner());
-    _setOwner(address(0));
-  }
-
-  function _setOwner(address newOwner) private {
-    address oldOwner = _owner;
-    _owner = newOwner;
-    emit OwnershipTransferred(oldOwner, newOwner);
-  }
-
-  /// @dev Returns the address of the current owner.
-  function owner() public view returns (address) {
-    return _owner;
-  }
-
-  //======== /OZ =========
 
   function minter() public view returns (address) {
     return _minter;
@@ -78,50 +36,6 @@ contract OurProxy is OurStorage {
     return _splitter;
   }
 
-  /// NOTE: If owner calls proxy, they are able to call OurMinter functions,
-  /// otherwise it acts like OurSplitter
-  fallback() external payable {
-    if (msg.sender == owner()) {
-      address _impl = minter();
-      assembly {
-        let ptr := mload(0x40)
-        calldatacopy(ptr, 0, calldatasize())
-        let result := delegatecall(gas(), _impl, ptr, calldatasize(), 0, 0)
-        let size := returndatasize()
-        returndatacopy(ptr, 0, size)
+  
 
-        switch result
-        case 0 {
-          revert(ptr, size)
-        }
-        default {
-          return(ptr, size)
-        }
-      }
-    } else {
-      address _impl = splitter();
-      assembly {
-        let ptr := mload(0x40)
-        calldatacopy(ptr, 0, calldatasize())
-        let result := delegatecall(gas(), _impl, ptr, calldatasize(), 0, 0)
-        let size := returndatasize()
-        returndatacopy(ptr, 0, size)
-
-        switch result
-        case 0 {
-          revert(ptr, size)
-        }
-        default {
-          return(ptr, size)
-        }
-      }
-    }
-  }
-
-  // Plain ETH transfers.
-  event ETHReceived(uint256 value);
-  receive() external payable {
-    emit ETHReceived(msg.value);
-    depositedInWindow += msg.value;
-  }
 }
