75c704d778d1119d6a1f8b0d3340cd0a09ae0814
nickadamson <naburner@protonmail.com>
2021-08-30 18:16:10 -0700

    store only essentials

--- contracts/OurPylon.sol
+++ contracts/OurPylon.sol
@@ -1,19 +1,80 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.4;
 
-import { OurStorage } from "./OurStorage.sol";
-import { OurManagement } from "./OurManagement.sol";
-import { OurIntrospector } from "./OurIntrospector.sol";
+import {OurStorage} from "./OurStorage.sol";
+import {OurManagement} from "./OurManagement.sol";
+import {OurIntrospector} from "./OurIntrospector.sol";
 
-contract OurPylon is 
-  OurStorage, 
-  OurManagement,
-  OurIntrospector 
-{
-  event ProxySetup(address indexed initiator, address[] owners, uint256 threshold, address initializer, address fallbackHandler);
+contract OurPylon is OurStorage, OurManagement, OurIntrospector {
+    address internal _splitter;
+    address internal _minter;
 
-  // This constructor ensures that this contract cannot be modified
-  constructor() {
-    threshold = 1;
-  }
+    event ProxySetup(
+        address indexed initiator,
+        address[] owners,
+        uint256 threshold,
+        address initializer,
+        address fallbackHandler
+    );
+
+    // This constructor ensures that this contract cannot be modified
+    constructor(address splitter_, address minter_) {
+        _splitter = splitter_;
+        _minter = minter_;
+        threshold = 1;
+    }
+
+    /// NOTE: If owner calls proxy, they are able to call OurMinter functions,
+    /// otherwise it acts like OurSplitter
+    fallback() external payable {
+        if (isOwner(msg.sender)) {
+            address _impl = minter();
+            assembly {
+                let ptr := mload(0x40)
+                calldatacopy(ptr, 0, calldatasize())
+                let result := delegatecall(
+                    gas(),
+                    _impl,
+                    ptr,
+                    calldatasize(),
+                    0,
+                    0
+                )
+                let size := returndatasize()
+                returndatacopy(ptr, 0, size)
+
+                switch result
+                case 0 {
+                    revert(ptr, size)
+                }
+                default {
+                    return(ptr, size)
+                }
+            }
+        } else {
+            address _impl = splitter();
+            assembly {
+                let ptr := mload(0x40)
+                calldatacopy(ptr, 0, calldatasize())
+                let result := delegatecall(
+                    gas(),
+                    _impl,
+                    ptr,
+                    calldatasize(),
+                    0,
+                    0
+                )
+                let size := returndatasize()
+                returndatacopy(ptr, 0, size)
+
+                switch result
+                case 0 {
+                    revert(ptr, size)
+                }
+                default {
+                    return(ptr, size)
+                }
+            }
+        }
+    }
 }
--- contracts/OurStorage.sol
+++ contracts/OurStorage.sol
@@ -8,18 +8,16 @@ pragma solidity 0.8.4;
  * @notice Modified: store addresses as constants, add _minter
  */
 contract OurStorage {
-  address internal _pylon;
-  address internal _splitter;
-  address internal _minter;
+    bytes32 public merkleRoot;
+    uint256 public currentWindow;
 
-  bytes32 public merkleRoot;
-  uint256 public currentWindow;
+    address internal _pylon;
 
-  /// @notice RINKEBY ADDRESS
-  address public constant wethAddress = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
+    /// @notice RINKEBY ADDRESS
+    address public constant wethAddress =
+        0xc778417E063141139Fce010982780140Aa0cD5Ab;
 
-
-  uint256[] public balanceForWindow;
-  mapping(bytes32 => bool) internal claimed;
-  uint256 internal depositedInWindow;
+    uint256[] public balanceForWindow;
+    mapping(bytes32 => bool) internal claimed;
+    uint256 internal depositedInWindow;
 }
