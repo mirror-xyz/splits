0757a4cdea567921520ff81e6f6b07b9b10720aa
nickadamson <naburner@protonmail.com>
2021-09-17 16:20:02 -0700

    fix: put ETHReceived event back in OurProxy

--- contracts/OurProxy.sol
+++ contracts/OurProxy.sol
@@ -19,6 +19,9 @@ interface IOurFactory {
  * & of course, @author OpenZeppelin
  */
 contract OurProxy is OurStorage {
+    // emits sender and value in receive() fallback
+    event ETHReceived(address indexed sender, uint256 value);
+
     constructor() {
         _pylon = IOurFactory(msg.sender).pylon();
         merkleRoot = IOurFactory(msg.sender).merkleRoot();
@@ -45,5 +48,11 @@ contract OurProxy is OurStorage {
                 return(ptr, size)
             }
         }
+    }
+
+    // Plain ETH transfers
+    receive() external payable {
+        depositedInWindow += msg.value;
+        emit ETHReceived(msg.sender, msg.value);
     }
 }
